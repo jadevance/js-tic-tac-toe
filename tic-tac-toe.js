a70ed4b3c6f7123581e92cd0b3e24ed6fb88e797
Jade Vance <jadenicolevance@gmail.com>
2016-06-30 14:10:53 -0700

    cribbed some CSS from the keyboard project to get started, will fix later. Added an event handler for on mouse click, adds a taco to the play space

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,47 @@
 function TicTacToe() {
+  //cheap trick to grab an instance ref of TicTacToe
+  var instance = this;
 
+  this.initialize = function() {
+    this._setupButtons();
+  };
+
+  this._setupButtons = function() {
+    $('button.space').click(function() {
+      var position = $(this).attr('class').split(' ')[1];
+      // check to see if that position is taken or not (check move method)
+        // if taken, alert user they can't make that move
+      // else
+      // pass which team is making the move
+      instance._playMove(position);
+      //check for win/lose/draw
+
+    });
+  };
+
+  this._playMove = function(position) {
+    var position = $('button.space.'+ position)
+    $(position).text('🌮');
+    // maybe remove that space from possible plays?
+  };
 }
 
-TicTacToe.prototype = {
-  
-}
+$(document).ready( function() {
+  var tictactoe = new TicTacToe();
+  tictactoe.initialize();
+});
+
+
+// what is a turn? 
+
+// determine which player has a turn 
+// select a space
+  // space available? 
+  // space advantage
+// claim space using player identifier
+// check win/loss state
+  // what is a win?
+  // what is a draw?
+  // what is a loss? 
+  // communicate outcome 
+  // continue play? 
